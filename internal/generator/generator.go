79174c095000c2efeb001956b9911a80460fce0f
Artem Murashko <artmurashko@yandex.ru>
2023-04-01 14:35:29 +0300

    [GOJHW-4]: Add order id generator

---
+++ internal/generator/generator.go
@@ -0,0 +1,26 @@
+package generator
+
+import (
+	"context"
+	"hw4/internal/model"
+)
+
+func OrderIDs(ctx context.Context) <-chan model.OrderID {
+	result := make(chan model.OrderID)
+	go func() {
+		defer close(result)
+
+		var counter uint64
+		for {
+			counter++
+
+			select {
+			case <-ctx.Done():
+				return
+			case result <- model.OrderID(counter):
+			}
+		}
+	}()
+
+	return result
+}
